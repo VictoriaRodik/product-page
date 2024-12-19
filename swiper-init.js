ed50ddba87fdec234e61a07c6f756804029c9f58
VictoriaRodik <victoriarodik@gmail.com>
2024-12-19 18:14:05 +0200

    styled the carousels

--- swiper-init.js
+++ swiper-init.js
@@ -1,8 +1,39 @@
-const swiper = new Swiper('.swiper', {
+const swiperLogos = new Swiper('.swiper-logos', {
     loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
   });
+
+  const swiperProducts = new Swiper('.swiper-products', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      init: function () {
+        const thumbnailsContainer = document.querySelector('.swiper-products .swiper-thumbnails');
+        this.slides.forEach((slide, index) => {
+          const img = slide.querySelector('img');
+          const thumbnail = document.createElement('img');
+          thumbnail.src = img.src;
+          thumbnail.alt = img.alt;
+          thumbnail.classList.add('thumbnail', 'w-6', 'h-6', 'object-cover', 'cursor-pointer');
+          thumbnail.dataset.index = index;
   
+          thumbnail.addEventListener('click', () => {
+            swiperProducts.slideToLoop(index); 
+          });
+  
+          thumbnailsContainer.appendChild(thumbnail);
+        });
+      },
+      slideChange: function () {
+        const thumbnails = document.querySelectorAll('.swiper-products .thumbnail');
+        thumbnails.forEach((thumb, index) => {
+          thumb.classList.toggle('active', index === this.realIndex);
+        });
+      },
+    },
+  });
